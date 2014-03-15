77f5d2dd6e5a2ce81fb2a70d0fce103803625402
kristiansthefan <kristiansthefan@192.168.1.100>
2014-03-15 11:11:38 -0430

    Revert "heroku"
    This reverts commit 34664a9c8dd0af6217385fb4695f38bcaa4e2aa6.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -60,7 +60,5 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.3.2"
 
         compile ':cache:1.0.1'
-        compile ':heroku:1.0.1'
-   compile ':cloud-support:1.0.8'
     }
 }
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -24,33 +24,20 @@ environments {
         }
     }
     production {
-//        dataSource {
-//            dbCreate = "update"
-//            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-//            pooled = true
-//            properties {
-//               maxActive = -1
-//               minEvictableIdleTimeMillis=1800000
-//               timeBetweenEvictionRunsMillis=1800000
-//               numTestsPerEvictionRun=3
-//               testOnBorrow=true
-//               testWhileIdle=true
-//               testOnReturn=true
-//               validationQuery="SELECT 1"
-//            }
-//        }
-        
         dataSource {
-        dbCreate = "update"
-        driverClassName = "org.postgresql.Driver"
-        dialect = org.hibernate.dialect.PostgreSQLDialect
-    
-        uri = new URI(System.env.DATABASE_URL?:"postgres://smepcvsprjtvqp:1TY2qzwV9sX0DMaWV1W4OHuQAo@ec2-54-197-250-40.compute-1.amazonaws.com:5432/d7grnn2s3cnmsv")
-
-        url = "jdbc:postgresql://"+uri.host+uri.path
-        username = uri.userInfo.split(":")[0]
-        password = uri.userInfo.split(":")[1]
-    }
-        
+            dbCreate = "update"
+            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            pooled = true
+            properties {
+               maxActive = -1
+               minEvictableIdleTimeMillis=1800000
+               timeBetweenEvictionRunsMillis=1800000
+               numTestsPerEvictionRun=3
+               testOnBorrow=true
+               testWhileIdle=true
+               testOnReturn=true
+               validationQuery="SELECT 1"
+            }
+        }
     }
 }
