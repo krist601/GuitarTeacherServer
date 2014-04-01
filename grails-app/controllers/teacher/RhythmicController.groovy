7f4e854f6a025d48f65dd1b6a260c6489b19e757
Eagle <Eagle@Eagle-PC>
2014-04-01 00:06:38 -0430

    To english

--- grails-app/controllers/teacher/RhythmicController.groovy
+++ grails-app/controllers/teacher/RhythmicController.groovy
@@ -12,90 +12,90 @@ class RhythmicController {
 
     def list(Integer max) {
         params.max = Math.min(max ?: 10, 100)
-        [ritmicaInstanceList: Rhythmic.list(params), ritmicaInstanceTotal: Ritmica.count()]
+        [rhythmicInstanceList: Rhythmic.list(params), rhythmicInstanceTotal: Ritmica.count()]
     }
 
     def create() {
-        [ritmicaInstance: new Rhythmic(params)]
+        [rhythmicInstance: new Rhythmic(params)]
     }
 
     def save() {
-        def ritmicaInstance = new Rhythmic(params)
-        if (!ritmicaInstance.save(flush: true)) {
-            render(view: "create", model: [ritmicaInstance: ritmicaInstance])
+        def rhythmicInstance = new Rhythmic(params)
+        if (!rhythmicInstance.save(flush: true)) {
+            render(view: "create", model: [rhythmicInstance: rhythmicInstance])
             return
         }
 
-        flash.message = message(code: 'default.created.message', args: [message(code: 'ritmica.label', default: 'Ritmica'), ritmicaInstance.id])
-        redirect(action: "show", id: ritmicaInstance.id)
+        flash.message = message(code: 'default.created.message', args: [message(code: 'rhythmic.label', default: 'Ritmica'), rhythmicInstance.id])
+        redirect(action: "show", id: rhythmicInstance.id)
     }
 
     def show(Long id) {
-        def ritmicaInstance = Rhythmic.get(id)
-        if (!ritmicaInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'ritmica.label', default: 'Ritmica'), id])
+        def rhythmicInstance = Rhythmic.get(id)
+        if (!rhythmicInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'rhythmic.label', default: 'Ritmica'), id])
             redirect(action: "list")
             return
         }
 
-        [ritmicaInstance: ritmicaInstance]
+        [rhythmicInstance: rhythmicInstance]
     }
 
     def edit(Long id) {
-        def ritmicaInstance = Rhythmic.get(id)
-        if (!ritmicaInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'ritmica.label', default: 'Ritmica'), id])
+        def rhythmicInstance = Rhythmic.get(id)
+        if (!rhythmicInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'rhythmic.label', default: 'Ritmica'), id])
             redirect(action: "list")
             return
         }
 
-        [ritmicaInstance: ritmicaInstance]
+        [rhythmicInstance: rhythmicInstance]
     }
 
     def update(Long id, Long version) {
-        def ritmicaInstance = Rhythmic.get(id)
-        if (!ritmicaInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'ritmica.label', default: 'Ritmica'), id])
+        def rhythmicInstance = Rhythmic.get(id)
+        if (!rhythmicInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'rhythmic.label', default: 'Ritmica'), id])
             redirect(action: "list")
             return
         }
 
         if (version != null) {
-            if (ritmicaInstance.version > version) {
-                ritmicaInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
-                          [message(code: 'ritmica.label', default: 'Ritmica')] as Object[],
+            if (rhythmicInstance.version > version) {
+                rhythmicInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+                          [message(code: 'rhythmic.label', default: 'Ritmica')] as Object[],
                           "Another user has updated this Ritmica while you were editing")
-                render(view: "edit", model: [ritmicaInstance: ritmicaInstance])
+                render(view: "edit", model: [rhythmicInstance: rhythmicInstance])
                 return
             }
         }
 
-        ritmicaInstance.properties = params
+        rhythmicInstance.properties = params
 
-        if (!ritmicaInstance.save(flush: true)) {
-            render(view: "edit", model: [ritmicaInstance: ritmicaInstance])
+        if (!rhythmicInstance.save(flush: true)) {
+            render(view: "edit", model: [rhythmicInstance: rhythmicInstance])
             return
         }
 
-        flash.message = message(code: 'default.updated.message', args: [message(code: 'ritmica.label', default: 'Ritmica'), ritmicaInstance.id])
-        redirect(action: "show", id: ritmicaInstance.id)
+        flash.message = message(code: 'default.updated.message', args: [message(code: 'rhythmic.label', default: 'Ritmica'), rhythmicInstance.id])
+        redirect(action: "show", id: rhythmicInstance.id)
     }
 
     def delete(Long id) {
-        def ritmicaInstance = Rhythmic.get(id)
-        if (!ritmicaInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'ritmica.label', default: 'Ritmica'), id])
+        def rhythmicInstance = Rhythmic.get(id)
+        if (!rhythmicInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'rhythmic.label', default: 'Ritmica'), id])
             redirect(action: "list")
             return
         }
 
         try {
-            ritmicaInstance.delete(flush: true)
-            flash.message = message(code: 'default.deleted.message', args: [message(code: 'ritmica.label', default: 'Ritmica'), id])
+            rhythmicInstance.delete(flush: true)
+            flash.message = message(code: 'default.deleted.message', args: [message(code: 'rhythmic.label', default: 'Ritmica'), id])
             redirect(action: "list")
         }
         catch (DataIntegrityViolationException e) {
-            flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'ritmica.label', default: 'Ritmica'), id])
+            flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'rhythmic.label', default: 'Ritmica'), id])
             redirect(action: "show", id: id)
         }
     }
